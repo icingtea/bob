f9b6b8d580b60bc018e6a9feaecd7e59550437bd
lalitm1004 <lalitm1004@gmail.com>
2025-07-13 20:10:02 +0530

    feat: add source_type field for chunk

--- src/models/chunk.py
+++ src/models/chunk.py
@@ -1,11 +1,16 @@
 from typing import List
 from pydantic import BaseModel
 
-from models.metadata import DocumentMetadata
+from models.metadata import DocumentMetadata, SourceType
 
 
 class Chunk(BaseModel):
+    """
+    Atomic document definition
+    """
+
     chunk_id: str
-    text: str
+    source_type: SourceType
     metadata: DocumentMetadata
+    text: str
     embeddings: List[float]
